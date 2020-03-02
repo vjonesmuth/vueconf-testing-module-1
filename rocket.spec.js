7bdb893c2e2a48b02eeb5c88d1d2900d11dec2c3
Vince Jones <vincejones@Vinces-MacBook-Pro.local>
2020-03-02 10:41:05 -0600

    Improve throw error test.

--- rocket.spec.js
+++ rocket.spec.js
@@ -1,5 +1,6 @@
 import { createRocket } from './rocket';
 
+// 5.2
 it('can add fuel', () => {
   const rocket = createRocket();
   rocket.addFuel(100);
@@ -7,6 +8,7 @@ it('can add fuel', () => {
   expect(rocket.fuel).toEqual(100);
 });
 
+// 5.3
 it('must have 40 tonnes of rocket fuel to have sufficient', () => {
   const rocketA = createRocket();
   rocketA.addFuel(39);
@@ -19,12 +21,14 @@ it('must have 40 tonnes of rocket fuel to have sufficient', () => {
   expect(rocketB.hasSufficientFuel()).toBeTruthy();
 });
 
+// 5.4
 it('cannot start engine without fuel', () => {
   const rocket = createRocket();
 
-  expect(() => rocket.startEngine()).toThrow();
+  expect(() => rocket.startEngine()).toThrow('Insufficient fuel');
 });
 
+// 5.5
 it('can start engine with sufficient fuel', () => {
   const rocket = createRocket();
   rocket.addFuel(200);
